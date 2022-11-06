c0763eedf3b5d4413155d78e4f43b1544bf547fc
tsseagull <tsseagull@icloud.com>
2022-11-06 21:58:00 +0800

    2022/11/6

---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,28 @@
+import { Component } from "react";
+import './card-list.styles.css';
+
+class CardList extends Component {
+
+  render() {
+		console.log('CardList rendered');
+		const { monsters } = this.props;
+    return (
+      <div className="card-list">
+        {monsters.map((monster) => { 
+					const { id, name, email } = monster;
+					return (
+					<div className='card-container'>
+						<img 
+							alt={`monster ${name}`} 
+							src={`https://robohash.org/${id}?set=set2&size=180x180`}
+						/>
+						<h2>{name}</h2>
+						<p>{email}</p>
+					</div>
+				)})}
+      </div>
+    );
+  }
+}
+
+export default CardList;
